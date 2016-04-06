116c7989898968ee221124f9bb1fe799eaea2cb1
Bruno Flávio <bmflavio@gmail.com>
2016-04-07 00:54:19 +0100

    in order to run the tests the generated css from scss needs to be in the APP_SRC folder.

--- tools/tasks/seed/process.scss.ts
+++ tools/tasks/seed/process.scss.ts
@@ -1,12 +1,13 @@
 import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import {join} from 'path';
-import {APP_SRC, DEV_DEST, TMP_DIR, ENV} from './../../config';
+import {APP_SRC, /*DEV_DEST, TMP_DIR, ENV*/} from './../../config';
 const plugins = <any>gulpLoadPlugins();
-const isProd = ENV === 'prod';
+//const isProd = ENV === 'prod';
 
 export = () => {
   return gulp.src(join(APP_SRC, '**', '*.scss'))
     .pipe(plugins.sass())
-    .pipe(gulp.dest(isProd ? TMP_DIR : DEV_DEST));
+    .pipe(gulp.dest(APP_SRC));
+    //.pipe(gulp.dest(isProd ? TMP_DIR : DEV_DEST));
 }
